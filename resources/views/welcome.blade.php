9889040a0af5c608f2b7005279b0a18d6cebe852
salvoab <salvatore.abate85@gmail.com>
2021-02-17 15:51:44 +0100

    Inizializzazione progetto Laravel e creazione layout di base

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title')
+    HOME
+@endsection
+
+@section('content')
+    <h1>Home page del Blog</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit doloremque dolor temporibus voluptates nam esse aliquam ullam earum dolores accusamus odio, dignissimos amet, voluptatum consequatur eum perspiciatis iure autem veritatis?</p>
+@endsection
