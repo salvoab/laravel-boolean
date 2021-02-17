630d4a9a8d00eff0c8ba5114037b7cc95e982de1
salvoab <salvatore.abate85@gmail.com>
2021-02-17 18:04:38 +0100

    Sostituita notazione [" "] con notazione freccia ->
    La notazione [" "]  funziona ma ho sostituito la notazione con la notazione freccia -> perché quest'ultima è la notazione usata nella documentazione di Laravel

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <h1>Articoli</h1>
     @foreach($posts as $post)
-        <h2>{{ $post["title"] }}</h2>
-        <p>{{ $post["body"] }}</p>
+        <h2>{{ $post->title }}</h2>
+        <p>{{ $post->body }}</p>
     @endforeach
 @endsection
